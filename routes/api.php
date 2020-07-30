d0c06c8cb9cc2b8c84fdc7d1ffa4350e39cef095
brenu <bvsousa64@gmail.com>
2020-07-30 11:23:05 -0300

    :construction: Create user controller as an API Resource

--- routes/api.php
+++ routes/api.php
@@ -31,11 +31,16 @@ Route::get('/test', function(Request $request) {
 });
 
 //Products Route
-Route::namespace('Api')->prefix('products')->group(function(){
-    Route::get('/', 'ProductController@index');
-    Route::get('/{id}', 'ProductController@show');
-    Route::post('/', 'ProductController@save');
-    Route::put('/', 'ProductController@update');
-    Route::patch('/', 'ProductController@update');
-    Route::delete('/{id}', 'ProductController@delete');
+Route::namespace('Api')->group(function(){
+    // Products routes
+    Route::prefix('products')->group(function() {
+        Route::get('/', 'ProductController@index');
+        Route::get('/{id}', 'ProductController@show');
+        Route::post('/', 'ProductController@save');
+        Route::put('/', 'ProductController@update');
+        Route::patch('/', 'ProductController@update');
+        Route::delete('/{id}', 'ProductController@delete');
+    });
+
+    Route::resource('/users', 'UserController');
 });
